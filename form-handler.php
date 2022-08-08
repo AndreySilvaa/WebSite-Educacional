f239864f6c55caa011c80ca5c06f92b2c222950c
Andrey Silva Lima <andreysl2001@gmail.com>
2022-08-08 18:56:23 -0300

    PHP atulizado

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'https://andreysilvaa.github.io/WebSite-Educacional/';
 
 $email_subject = 'New Form Submission'
 
